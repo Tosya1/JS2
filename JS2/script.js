13520b0cae2f3fb1caa89bdc1d9a98d759c8f1ec
Tosya1 <95938909+Tosya1@users.noreply.github.com>
2022-07-01 01:10:03 +0300

    Add files via upload

--- JS2/script.js
+++ JS2/script.js
@@ -9,6 +9,53 @@ function service(url) {
 }
 
 window.onload = () => {
+    Vue.component('good', {
+        props: [
+            'item'
+        ],
+        template: `
+        <div class=" goods-item">
+        <h3>{{ item.product_name }}</h3>
+        <p>{{ item.price }}</p>
+        </div>
+        `
+    })
+
+    Vue.component('custom-button', {
+        template: `
+        <button class="cart-button" type="button" @click="$emit('click')">
+        <slot></slot>
+        </button>
+        `
+    })
+
+    Vue.component('basket', {
+        props: [
+            'calculatePrice'
+        ],
+        template: `
+        <div class="basket">
+            <div class="basket-header">
+                <div class="close-basket" @click="$emit('close')">
+                    <i class="fa-solid fa-xmark"></i>
+                </div>
+            </div>
+            <div class="basket-body"></div>
+            <div class="basket-footer">  Всего товаров на сумму  <span>{{ calculatePrice}}</span></div>
+        </div>
+        `
+    })
+
+    Vue.component('custom-input', {
+        props: ['value'],
+        template: `
+        <input type="text" class="goods-search"
+            v-bind:value="value"
+            v-on:input="$emit('input', $event.target.value)"
+        >
+        `
+    })
+
     const app = new Vue({
         el: '#root',
         data: {
@@ -40,7 +87,7 @@ window.onload = () => {
                     return product_name.match(new RegExp(this.search, 'i'));
                 })
             },
-            
+
             calculatePrice() {
                 return this.items.reduce((acc, { price }) => acc + price, 0);
             }
